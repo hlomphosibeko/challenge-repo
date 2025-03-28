0c000f6510536294a6c585e5314a070dda8cd0e1
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-03-28 15:39:56 +0000

    Create content and add images to the site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import css from './App.module.css';
+import Content from './components/Content';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
@@ -7,6 +8,7 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className={css.App}>
+      <Content />
       <NavBarForm />
       {/* <NavBarSimple /> */}
       <Sidebar />
---
+++ my-app/src/components/Content.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {savedPosts} from '../posts.json';
+import css from './css/Content.module.css';
+import PostItem from './PostItem';
+
+class Content extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className={css.Content}>
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
+                <div className={css.SearchResults}>
+                    <PostItem savedPosts={savedPosts} />
+                </div>
+            </div>
+        )
+    }    
+}
+
+export default Content;
