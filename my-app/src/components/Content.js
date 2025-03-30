4eb5d844bfc3450a05b6508c311bcc894290a6fb
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-03-30 22:20:44 +0000

    Create the search bar using search features

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,19 +2,50 @@ import React, { Component } from 'react';
 import {savedPosts} from '../posts.json';
 import css from './css/Content.module.css';
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 class Content extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isLoaded: 'false',
+            posts: [],
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts,
+            })
+        }, 2000)
+    }
+    handleChange = (event) => {
+        const name = event.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name);
+        })
+        this.setState({
+            posts: filteredPosts
+        })
     }
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor='searchInput'>Search:</label>
+                        <input onChange={(event) => this.handleChange(event)} id='searchInput' type='search' placeholder='By Author' />
+                        <h4>posts found: {this.state.posts.length}</h4>
+                    </form>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        this.state.isLoaded ? <PostItem savedPosts={this.state.posts} /> : <Loader  />
+                    }
+                    
                 </div>
             </div>
         )
