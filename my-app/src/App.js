39e0ceb6134712b8300142433556aa279f6764e0
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-03-31 20:49:54 +0000

    Add Hooks to content page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import css from './App.module.css';
 import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className={css.App}>
       <Content />
+      <ContentHooks />
       <NavBarForm />
       {/* <NavBarSimple /> */}
       <Sidebar />
