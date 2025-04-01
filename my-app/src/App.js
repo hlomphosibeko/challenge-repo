a3a5f126b0cba2f99f9d0fb0a84514f49890f387
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-04-01 21:13:30 +0000

    Add API to the content to be able to request data from the web

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import css from './App.module.css';
-import Content from './components/Content';
-import ContentHooks from './components/ContentHooks';
+// import Content from './components/Content';
+// import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
+// import ContentHooks from './components/ContentHooks';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
@@ -9,8 +11,10 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className={css.App}>
-      <Content />
-      <ContentHooks />
+      {/* <Content /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
+      {/* <ContentHooks /> */}
       <NavBarForm />
       {/* <NavBarSimple /> */}
       <Sidebar />
